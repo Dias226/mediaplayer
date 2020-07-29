5fafa70df85b711c182d7a4e03ab91c32724eb00
Dias <said.ahmetov@list.ru>
2020-07-29 21:41:18 +0600

    add radio player

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -1,19 +1,22 @@
 export const videoPlayerInit = () => {
+  const videoPlayer = document.querySelector(".video-player");
+  const videoButtonPlay = document.querySelector(".video-button__play");
+  const videoButtonStop = document.querySelector(".video-button__stop");
+  const videoProgress = document.querySelector(".video-progress");
+  const videoTimePassed = document.querySelector(".video-time__passed");
+  const videoTimeTotal = document.querySelector(".video-time__total");
+  const videoVolume = document.querySelector('.video-volume');
+  const videoFullscreen = document.querySelector('.video-fullscreen');
 
-  const videoPlayer = document.querySelector('.video-player');
-  const videoButtonPlay = document.querySelector('.video-button__play');
-  const videoButtonStop = document.querySelector('.video-button__stop');
-  const videoProgress = document.querySelector('.video-progress');
-  const videoTimePassed = document.querySelector('.video-time__passed');
-  const videoTimeTotal = document.querySelector('.video-time__total');
+
 
   const toggleIcon = () => {
     if (videoPlayer.paused) {
-      videoButtonPlay.classList.remove('fa-pause');
-      videoButtonPlay.classList.add('fa-play');
+      videoButtonPlay.classList.remove("fa-pause");
+      videoButtonPlay.classList.add("fa-play");
     } else {
-      videoButtonPlay.classList.add('fa-pause');
-      videoButtonPlay.classList.remove('fa-play');
+      videoButtonPlay.classList.add("fa-pause");
+      videoButtonPlay.classList.remove("fa-play");
     }
   };
 
@@ -30,17 +33,17 @@ export const videoPlayerInit = () => {
     videoPlayer.currentTime = 0;
   };
 
-  const addZero = n => n < 10 ? '0' + n : n;
+  const addZero = (n) => (n < 10 ? "0" + n : n);
 
-  videoPlayer.addEventListener('click', togglePlay);
-  videoButtonPlay.addEventListener('click', togglePlay);
+  videoPlayer.addEventListener("click", togglePlay);
+  videoButtonPlay.addEventListener("click", togglePlay);
 
-  videoPlayer.addEventListener('play', toggleIcon);
-  videoPlayer.addEventListener('pause', toggleIcon);
+  videoPlayer.addEventListener("play", toggleIcon);
+  videoPlayer.addEventListener("pause", toggleIcon);
 
-  videoButtonStop.addEventListener('click', stopPlay);
+  videoButtonStop.addEventListener("click", stopPlay);
 
-  videoPlayer.addEventListener('timeupdate', () => {
+  videoPlayer.addEventListener("timeupdate", () => {
     const currentTime = videoPlayer.currentTime;
     const duration = videoPlayer.duration;
 
@@ -52,14 +55,28 @@ export const videoPlayerInit = () => {
     let minuteTotal = Math.floor(duration / 60);
     let secondsTotal = Math.floor(duration % 60);
 
-    videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(secondsPassed)}`;
-    videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(secondsTotal)}`;
+    videoTimePassed.textContent = `${addZero(minutePassed)}:${addZero(
+      secondsPassed
+    )}`;
+    videoTimeTotal.textContent = `${addZero(minuteTotal)}:${addZero(
+      secondsTotal
+    )}`;
   });
 
-  videoProgress.addEventListener('change', () => {
+  videoProgress.addEventListener("input", () => {
     const duration = videoPlayer.duration;
     const value = videoProgress.value;
 
     videoPlayer.currentTime = (value * duration) / 100;
   });
+
+  videoFullscreen.addEventListener('click', () => {
+    videoPlayer.requestFullscreen();
+  });
+
+  videoVolume.addEventListener('input', () => {
+    videoPlayer.volume = videoVolume.value / 100;
+  });
+  videoPlayer.volume = 0.5;
+  videoVolume.value = videoPlayer.volume * 100;
 };
